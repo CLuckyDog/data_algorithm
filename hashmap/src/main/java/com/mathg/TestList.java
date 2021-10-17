d7662e988ff055215778e39f91d4152b4162dbf7
unknown <Administrator@note190342.Hoperun.com>
2021-10-17 11:03:20 +0800

    提交红黑树左旋右旋图片说明

---
+++ hashmap/src/main/java/com/mathg/TestList.java
@@ -0,0 +1,23 @@
+package com.mathg;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * \* Created with IntelliJ IDEA.
+ * \* User: 潘忠健
+ * \* Date: 2021/4/26
+ * \* Time: 9:24
+ * \* Description:
+ * \
+ */
+public class TestList {
+    @Test
+    public void test1(){
+        List<String> list = new LinkedList<>();
+
+    }
+}
